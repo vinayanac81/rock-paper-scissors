92e5c618fa2fb68bbf90e626406183d546df661d
Vinayan AC <vinayanac7777@gmail.com>
2024-11-13 19:49:42 +0530

    image updated

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App() {
       callSetTimeout();
     } else if (userClicked === 1 && computerClicked === 2) {
       setcomputerPoint(computerPoint + 1);
-      setcomputerImage(`image${computerClicked}`);
-      setuserImage(`image${userClicked}`);
+      setcomputerImage(`img${computerClicked}`);
+      setuserImage(`img${userClicked}`);
       setuserWin(false);
       setbothSame(false);
       setpickedOne(true);
